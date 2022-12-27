87135282a80d800601e87ee3f66c70bc7a1e4228
Francesco Balestrieri <bale@balenet.com>
2022-12-27 08:23:05 +0100

    Refactor logic into StoryBrain struct

--- Destini-iOS13/Controller/ViewController.swift
+++ Destini-iOS13/Controller/ViewController.swift
@@ -9,6 +9,8 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var story = StoryBrain()
 
     @IBOutlet weak var titleLabel: UILabel!
     @IBOutlet weak var choice1Button: UIButton!
@@ -17,52 +19,17 @@ class ViewController: UIViewController {
     
     @IBAction func choiceSelected(_ sender: UIButton) {
         if sender == choice1Button {
-            currentChapter = chapters[currentChapter].choice1Destination
+            story.chooseOne()
         } else {
-            currentChapter = chapters[currentChapter].choice2Destination
+            story.chooseTwo()
         }
         updateUI()
     }
     
-
-    let chapters = [
-    Story(
-                title: "Your car has blown a tire on a winding road in the middle of nowhere with no cell phone reception. You decide to hitchhike. A rusty pickup truck rumbles to a stop next to you. A man with a wide brimmed hat with soulless eyes opens the passenger door for you and asks: 'Need a ride, boy?'.",
-                choice1: "I'll hop in. Thanks for the help!", choice1Destination: 2,
-                choice2: "Better ask him if he's a murderer first.", choice2Destination: 1
-            ),
-            Story(
-                title: "He nods slowly, unfazed by the question.",
-                choice1: "At least he's honest. I'll climb in.", choice1Destination: 2,
-                choice2: "Wait, I know how to change a tire.", choice2Destination: 3
-            ),
-            Story(
-                title: "As you begin to drive, the stranger starts talking about his relationship with his mother. He gets angrier and angrier by the minute. He asks you to open the glovebox. Inside you find a bloody knife, two severed fingers, and a cassette tape of Elton John. He reaches for the glove box.",
-                choice1: "I love Elton John! Hand him the cassette tape.", choice1Destination: 5,
-                choice2: "It's him or me! You take the knife and stab him.", choice2Destination: 4
-            ),
-            Story(
-                title: "What? Such a cop out! Did you know traffic accidents are the second leading cause of accidental death for most adult age groups?",
-                choice1: "The", choice1Destination: 0,
-                choice2: "End", choice2Destination: 0
-            ),
-            Story(
-                title: "As you smash through the guardrail and careen towards the jagged rocks below you reflect on the dubious wisdom of stabbing someone while they are driving a car you are in.",
-                choice1: "The", choice1Destination: 0,
-                choice2: "End", choice2Destination: 0
-            ),
-            Story(
-                title: "You bond with the murderer while crooning verses of 'Can you feel the love tonight'. He drops you off at the next town. Before you go he asks you if you know any good places to dump bodies. You reply: 'Try the pier.'",
-                choice1: "The", choice1Destination: 0,
-                choice2: "End", choice2Destination: 0
-            ) ]
-
-    var currentChapter = 0
-    
     func updateUI() {
-        titleLabel.text = chapters[currentChapter].title
-        choice1Button.setTitle(chapters[currentChapter].choice1, for: .normal)
-        choice2Button.setTitle(chapters[currentChapter].choice2, for: .normal)
+        titleLabel.text = story.getTitle()
+        choice1Button.setTitle(story.getChoice1Text(), for: .normal)
+        choice2Button.setTitle(story.getChoice2Text(), for: .normal)
     }
 
     override func viewDidLoad() {
